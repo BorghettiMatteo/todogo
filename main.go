9d85ce580e9b6e6bf92b761eb6173af5c81fefba
BorghettiMatteo <matteo.borghe@gmail.com>
2023-05-25 11:59:02 +0200

    add gin example

--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("dioporco perchè  non riesco a committare?")
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
