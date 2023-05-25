76c3de1f2d21bafe129f4fed27740cabd3dc164d
BorghettiMatteo <matteo.borghe@gmail.com>
2023-05-25 11:35:44 +0200

    add gin to project

--- todo/todo.go
+++ todo/todo.go
@@ -1,11 +1,14 @@
 package todo
 
-import "time"
+import (
+	"time"
+)
 
 type ToDo struct {
-	ID        string    `json:"id"`
-	Activity  string    `json:"activity"`
-	IsDone    bool      `json:"isdone"`
-	StartTime time.Time `json:"time"`
-	Owner     string    `json:"owner"`
+	ID            string    `json:"id"`
+	Activity      string    `json:"activity"`
+	ActivityOwner string    `json:"activityowner"`
+	Creation      time.Time `json:"creation"`
+	Expiration    time.Time `json:"expiration"`
+	IsDone        bool      `json:"isdone"`
 }
