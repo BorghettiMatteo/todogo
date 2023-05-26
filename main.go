4e08ec929ffb0904e70f2be90a8b172bf887fc29
Borghetti <matteo.borghetti@elmec.it>
2023-05-26 12:26:44 +0200

    aggiunto

--- main.go
+++ main.go
@@ -7,7 +7,10 @@ import (
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
+
+// prima faccio partire GIn, poi nel caso
 
 /*
 	func setupRouter() *gin.Engine {
@@ -25,7 +28,13 @@ import (
 func main() {
 
 	connStr := "user=matteo dbname=ToDo password=password host=172.28.120.162 port=5432 sslmode=disable"
-	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+		NowFunc: func() time.Time {
+			ti, _ := time.LoadLocation("Europe/Rome")
+			return time.Now().In(ti)
+		},
+	})
 
 	if err != nil {
 		fmt.Println("si è schienata la configurazione con il db")
@@ -63,6 +72,12 @@ func main() {
 	}
 
 	db.Debug().Create(&samplequery)
+	newtodo := todo.ToDo{
+		Id: 6,
+	}
+
+	db.Take(&newtodo)
+	fmt.Printf("new: %v\n", newtodo)
 	//db.Last(&samplequery)
 
 }
